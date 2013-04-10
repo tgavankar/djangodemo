de9694af67e78dca78a1f23ebb62a947d1cdc839
Tanay Gavankar <tgavankar@gmail.com>
2013-04-09 23:02:44 -0400

    enable user login/logout

--- pollme/accounts/urls.py
+++ pollme/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 
 from accounts import views
 
@@ -7,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     # ex: /accounts/register/
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 )
