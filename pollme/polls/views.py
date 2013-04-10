d7093513d93ed333a29d5bbeff516d86c1389c0e
Tanay Gavankar <tgavankar@gmail.com>
2013-04-09 20:51:47 -0400

    index template

--- pollme/polls/views.py
+++ pollme/polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+from polls.models import Poll
 
 def index(request):
-	return HttpResponse("Welcome to Poll.me!")
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
 	return HttpResponse("Showing details of poll: %s" % poll_id)
