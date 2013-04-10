f8d983168cf172c2f0df3e84c5b64637c6da8116
Tanay Gavankar <tgavankar@gmail.com>
2013-04-09 20:42:06 -0400

    setup nonfunctional views

--- pollme/polls/views.py
+++ pollme/polls/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+	return HttpResponse("Welcome to Poll.me!")
+
+def detail(request, poll_id):
+	return HttpResponse("Showing details of poll: %s" % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("Showing results of poll: %s" % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("Vote on poll: %s" % poll_id)
